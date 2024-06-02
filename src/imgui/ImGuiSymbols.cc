9e4545b8725660a90361dd4c859370ba48a96618
Pedro de Medeiros <pedro.medeiros@gmail.com>
2024-06-02 10:05:31 -0300

    move code to separate drawSlots function

--- src/imgui/ImGuiSymbols.cc
+++ src/imgui/ImGuiSymbols.cc
@@ -130,6 +130,36 @@ static void checkSort(const SymbolManager& manager, std::vector<SymbolRef>& symb
 	}
 }
 
+static void drawSlots(ImGuiManager& manager)
+{
+	auto flags = ImGuiTableFlags_ContextMenuInBody;
+	bool ss00, ss10, ss20, ss30;
+	im::Table("##slots-subslots", 4, flags, {0, 100}, [&]{
+		ImGui::TableSetupColumn("slot 0", ImGuiTableColumnFlags_WidthFixed);
+		ImGui::TableSetupColumn("slot 1", ImGuiTableColumnFlags_WidthFixed);
+		ImGui::TableSetupColumn("slot 2", ImGuiTableColumnFlags_WidthFixed);
+		ImGui::TableSetupColumn("slot 3", ImGuiTableColumnFlags_WidthFixed);
+		for (auto row = 0; row < 4; ++row) {
+			if (ImGui::TableNextColumn()) { // slot 0
+				im::ScopedFont sf(manager.fontMono);
+				ImGui::Checkbox(tmpStrCat("0-", row).c_str(), &ss00);
+			}
+			if (ImGui::TableNextColumn()) { // slot 1
+				im::ScopedFont sf(manager.fontMono);
+				ImGui::Checkbox(tmpStrCat("1-", row).c_str(), &ss10);
+			}
+			if (ImGui::TableNextColumn()) { // slot 2
+				im::ScopedFont sf(manager.fontMono);
+				ImGui::Checkbox(tmpStrCat("2-", row).c_str(), &ss20);
+			}
+			if (ImGui::TableNextColumn()) { // slot 3
+				im::ScopedFont sf(manager.fontMono);
+				ImGui::Checkbox(tmpStrCat("3-", row).c_str(), &ss30);
+			}
+		}
+	});
+}
+
 template<bool FILTER_FILE>
 static void drawTable(ImGuiManager& manager, const SymbolManager& symbolManager, std::vector<SymbolRef>& symbols, const std::string& file = {})
 {
@@ -197,34 +227,7 @@ static void drawTable(ImGuiManager& manager, const SymbolManager& symbolManager,
 				if (ImGui::IsItemClicked(ImGuiMouseButton_Right)) {
 					ImGui::OpenPopup(symNameMenu);
 				}
-				im::Popup(symNameMenu, [&]{
-					auto flags = ImGuiTableFlags_ContextMenuInBody;
-					bool ss00, ss10, ss20, ss30;
-					im::Table("##slots-subslots", 4, flags, {0, 100}, [&]{
-						ImGui::TableSetupColumn("slot 0", ImGuiTableColumnFlags_WidthFixed);
-						ImGui::TableSetupColumn("slot 1", ImGuiTableColumnFlags_WidthFixed);
-						ImGui::TableSetupColumn("slot 2", ImGuiTableColumnFlags_WidthFixed);
-						ImGui::TableSetupColumn("slot 3", ImGuiTableColumnFlags_WidthFixed);
-						for (auto row = 0; row < 4; ++row) {
-							if (ImGui::TableNextColumn()) { // slot 0
-								im::ScopedFont sf(manager.fontMono);
-								ImGui::Checkbox(tmpStrCat("0-", row).c_str(), &ss00);
-							}
-							if (ImGui::TableNextColumn()) { // slot 1
-								im::ScopedFont sf(manager.fontMono);
-								ImGui::Checkbox(tmpStrCat("1-", row).c_str(), &ss10);
-							}
-							if (ImGui::TableNextColumn()) { // slot 2
-								im::ScopedFont sf(manager.fontMono);
-								ImGui::Checkbox(tmpStrCat("2-", row).c_str(), &ss20);
-							}
-							if (ImGui::TableNextColumn()) { // slot 3
-								im::ScopedFont sf(manager.fontMono);
-								ImGui::Checkbox(tmpStrCat("3-", row).c_str(), &ss30);
-							}
-						}
-					});
-				});
+				im::Popup(symNameMenu, [&]{ drawSlots(manager); });
 			}
 			if (!FILTER_FILE && ImGui::TableNextColumn()) { // file
 				ImGui::TextUnformatted(sym.file(symbolManager));
